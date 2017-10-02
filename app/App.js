e2789f37f239a3dee321ac220b59845a83b95c73
vikramsunilpatil <vsp2293@gmail.com>
2017-10-02 22:41:37 +0530

    using maps instead of list for randomization

--- app/App.js
+++ app/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      random_numbers: [],
+      random_numbers: {},
       max_count: 5000,
       display_value: null
     };
@@ -18,15 +18,16 @@ export default class App extends React.Component {
   }
 
   findRandomNumber() {
-    if (!this.state.random_numbers.length) {
+    if (!Object.keys(this.state.random_numbers).length) {
       this.initializeList();
     } else {
-      let idx = Math.floor(Math.random() * this.state.random_numbers.length);
+      let keys = Object.keys(this.state.random_numbers);
+      let idx = Math.floor(Math.random() * keys.length);
       this.setState({
-        display_value: this.state.random_numbers[idx]
+        display_value: this.state.random_numbers[keys[idx]]
       }, () => {
         let random_numbers = this.state.random_numbers;
-        random_numbers.splice(idx, 1);
+        delete random_numbers[keys[idx]];
         this.setState({
           random_numbers: random_numbers
         });
@@ -39,12 +40,12 @@ export default class App extends React.Component {
     this.setState({
       display_value: initialDisplayValue
     });
-    let random_numbers = [];
+    let random_numbers = {};
     for (let i = 0; i < this.state.max_count; i++) {
       if (i === initialDisplayValue - 1) {
         continue;
       }
-      random_numbers.push(i+1);
+      random_numbers[i+1] = i+1;
     }
     this.setState({
       random_numbers: random_numbers
